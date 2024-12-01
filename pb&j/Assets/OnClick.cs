e3a16a213663ab9a5075049d8be710b862c4a51a
li69sus <li69@sussex.ac.uk>
2024-12-01 00:32:20 +0000

    save 2
    added song (my clematis)

--- pb&j/Assets/OnClick.cs
+++ pb&j/Assets/OnClick.cs
@@ -8,8 +8,15 @@ public class OnClick : MonoBehaviour
     public bool Touching;
     public KeyCode Arrow;
     public GameObject Player;
+    public AudioSource OnHit;
 
     // Update is called once per frame
+    IEnumerator Start()
+    {
+        yield return new WaitForSecondsRealtime(0.05f);
+        OnHit.Play();
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(Arrow) && Touching)
@@ -24,7 +31,7 @@ public class OnClick : MonoBehaviour
     IEnumerator ClickedSpaceAtTheMoment()
     {
         ClickedSpace = true;
-        yield return new WaitForSecondsRealtime(0.05f);
+        yield return new WaitForSecondsRealtime(0.01f);
         ClickedSpace = false;
     }
 
